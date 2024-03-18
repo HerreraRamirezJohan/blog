333f41857d5a2c590334f98c9b33aff6f658f478
Johan Herrera <johanherreraramirez@outlook.com>
2024-03-18 10:01:21 -0600

    Interface dashboard page & Migrations
    1. Modify app.blade.php and guest template for the new navbar
    2. Modify tailwind css to fix container for login&register
    3. Delete tailwind css to remove shadows
    4. Create folder partials to put secctions of html code
    5. Make firs migrations for jetstream
    6. Make migrations for the first functional reqiurement (make post with category)

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="text-sm space-x-4 flex items-center border-t border-gray-100 flex-wrap justify-center py-4 bg-black text-white">
+    <a class=" hover:text-yellow-500" href="">About Us</a>
+    <a class=" hover:text-yellow-500" href="">Help</a>
+    <a class=" hover:text-yellow-500" href="">Login</a>
+    <a class=" hover:text-yellow-500" href="">Explore</a>
+</footer>
