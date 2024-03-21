c45e155e5ddf13143f03a8cd08cfa9ea87fa0116
Herrera Johan <johanherreraramirez@outlook.com>
2024-03-20 22:04:05 -0600

    Posts and navegations links
    1. Modify fortify, change route HOME to /
    2. Change the navbars link with the blade component and add wire:navegation for no reload page.
    3. Add livewire: in the header to update when delete profile picture
    4. Create blog (posts) route posts.index view
    5. Create post-item component
    6. Show a excerpt of the post
    7. Show the reading time of each post, along with the average read time of Google.
    8. Create the cast and business rules for the previous requirements.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,15 +5,34 @@ namespace App\Models;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
     use HasFactory;
 
+    //Cast
+    public $casts = [
+        'published_at' => 'datetime',
+    ];
+    //Filters
     public function scopePublished($query){
         $query->where('published_at', '<=', Carbon::now());
     }
     public function scopeFeatured($query){
         $query->where('featured', true);
+    }
+    //Relations
+    public function author(){
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    //Business rules
+    public function getExcerptBody(){
+        return Str::limit(strip_tags($this->body), 150); //strip_tags remove html tags
+    }
+    public function getReadingTime(){
+        $min = round(str_word_count($this->body) / 250) < 1 ; //The number 250 is the average that show google
+        return $min < 1 ? 1 : $min;
     }
 }
